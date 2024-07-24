d7735febb43ec90752f4da45f33bf470e493b285
andre-amato <andreamatoprs@gmail.com>
2024-07-24 11:52:19 -0300

    feat: new submit button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import Button from './components/Button';
 import Header from './components/Header';
 
 const App = () => {
@@ -6,6 +7,7 @@ const App = () => {
       <Header />
       <main className='p-4'>
         <p className='text-gray-700'>This is your VendoDrives app!</p>
+        <Button />
       </main>
     </div>
   );
