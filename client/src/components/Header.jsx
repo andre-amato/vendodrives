058d682402b29155c480de9f2f1d6ad7d55c0931
andre-amato <andreamatoprs@gmail.com>
2024-07-24 12:17:13 -0300

    fix: header on messages

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
-import { Link, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 const Header = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleLogout = () => {
     // For the fake logout, we'll just navigate to the login page
@@ -12,9 +14,15 @@ const Header = () => {
     <header className='bg-blue-500 text-white p-4 flex justify-between items-center'>
       <h1 className='text-2xl'>Welcome to VendoDrives</h1>
       <div>
-        <Link to='/messages' className='mr-4 hover:underline'>
-          Messages
-        </Link>
+        {location.pathname === '/messages' ? (
+          <Link to='/main' className='mr-4 hover:underline'>
+            Main
+          </Link>
+        ) : (
+          <Link to='/messages' className='mr-4 hover:underline'>
+            Messages
+          </Link>
+        )}
         <button onClick={handleLogout} className='hover:underline'>
           Logout
         </button>
