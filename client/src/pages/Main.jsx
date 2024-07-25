a8e99649145c0d75be67948366374a4049c0a269
andre-amato <andreamatoprs@gmail.com>
2024-07-25 06:33:27 -0300

    feat: search bar on header

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -5,21 +5,33 @@ import Header from '../components/Header';
 
 const Main = () => {
   const [cards, setCards] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   const handleFormSubmit = (carDetails) => {
     setCards([...cards, carDetails]);
   };
 
+  const handleSearch = (query) => {
+    setSearchQuery(query.toLowerCase());
+  };
+
+  const filteredCards = cards.filter((card) =>
+    card.title.toLowerCase().includes(searchQuery)
+  );
+
   return (
     <div className='main-container'>
-      <Header className='sticky top-0 bg-white shadow-md z-30' />
+      <Header
+        onSearch={handleSearch}
+        className='sticky top-0 bg-white shadow-md z-30'
+      />
       <main className='p-4'>
         <div className='flex flex-col sm:flex-row'>
           <div className='form-container sm:sticky top-4 w-full sm:w-1/3 h-fit p-4 overflow-y-auto bg-white shadow-md z-20'>
             <CarForm onSubmit={handleFormSubmit} />
           </div>
           <div className='card-container flex flex-col sm:flex-row flex-wrap justify-end ml-auto w-full sm:w-2/3'>
-            {cards.map((card, index) => (
+            {filteredCards.map((card, index) => (
               <Card key={index} carDetails={card} />
             ))}
           </div>
