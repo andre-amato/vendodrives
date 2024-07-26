f6a9ad94cb5c3eeb7b27f3c1a7314c29d32129d1
andre-amato <andreamatoprs@gmail.com>
2024-07-26 08:01:31 -0300

    fix: card image

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -23,10 +23,13 @@ const Card = ({ carDetails }) => {
       </div>
       <div className='px-6 pt-4 pb-2'>
         <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
-          #example
+          #cheapcars
         </span>
         <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
-          #tailwindcss
+          #Volkswagen
+        </span>
+        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'>
+          #Easy
         </span>
       </div>
     </div>
