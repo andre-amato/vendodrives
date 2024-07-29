0b7ce6f3c0bfd96617a211a07ea6f9803dcf8b10
andre-amato <andreamatoprs@gmail.com>
2024-07-29 16:16:41 -0300

    feat: login with text

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -61,10 +61,10 @@ const Login = () => {
   };
 
   return (
-    <div className='relative flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+    <div className='relative min-h-screen bg-gray-100'>
       <Header showNavLinks={false} style={{ height: '80px' }} />
       <div
-        className='absolute inset-0 bg-gradient-to-b from-blue-200 t-blue-500'
+        className='absolute inset-0 bg-gradient-to-b from-blue-200 to-blue-500'
         style={{
           backgroundImage: `url(${racingFlagLogin})`,
           backgroundSize: '500px 500px',
@@ -72,58 +72,77 @@ const Login = () => {
           filter: 'invert(30%) brightness(100%) contrast(50%)',
         }}
       ></div>
-      <div className='relative bg-white p-8 rounded-2xl shadow-md w-80 mt-8 z-10'>
-        <h2 className='text-2xl font-bold mb-6 text-center'>
-          {isRegistering ? 'Register' : 'Login'}
-        </h2>
-        <form onSubmit={isRegistering ? handleRegister : handleLogin}>
-          {isRegistering && (
+      <div className='relative z-10 flex flex-col lg:flex-row items-center justify-center lg:justify-between lg:items-start lg:px-20 lg:py-12'>
+        <div className='hidden lg:flex flex-col items-start space-y-6 bg-blue-600 text-white p-8 rounded-xl w-full lg:w-1/2'>
+          <h1 className='text-3xl font-medium'>
+            Affordable Wheels, Real Deals
+          </h1>
+          <h2 className='text-xl'>
+            Connect to buy and sell cheap cars in hard economic times!
+          </h2>
+          <ul className='list-disc pl-5 mb-6 text-lg'>
+            <li>Wide selection of affordable cars</li>
+            <li>Safe and trusted community</li>
+            <li>Easy and quick transactions</li>
+          </ul>
+          <p className='text-lg'>
+            Join the VendoDrives community today and drive your dream car at a
+            dream price!
+          </p>
+        </div>
+        <div className='relative bg-white p-8 rounded-2xl shadow-md w-80 mt-8 lg:mt-0 lg:w-96 lg:ml-8'>
+          <h2 className='text-2xl font-bold mb-6 text-center'>
+            {isRegistering ? 'Register' : 'Login'}
+          </h2>
+          <form onSubmit={isRegistering ? handleRegister : handleLogin}>
+            {isRegistering && (
+              <div className='mb-4'>
+                <label className='block text-gray-700'>Name</label>
+                <input
+                  type='text'
+                  className='w-full p-2 border border-gray-300 rounded-lg mt-1'
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+            )}
             <div className='mb-4'>
-              <label className='block text-gray-700'>Name</label>
+              <label className='block text-gray-700'>Email</label>
               <input
-                type='text'
+                type='email'
                 className='w-full p-2 border border-gray-300 rounded-lg mt-1'
-                value={name}
-                onChange={(e) => setName(e.target.value)}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
               />
             </div>
-          )}
-          <div className='mb-4'>
-            <label className='block text-gray-700'>Email</label>
-            <input
-              type='email'
-              className='w-full p-2 border border-gray-300 rounded-lg mt-1'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className='mb-6'>
-            <label className='block text-gray-700'>Password</label>
-            <input
-              type='password'
-              className='w-full p-2 border border-gray-300 rounded-lg mt-1'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
+            <div className='mb-6'>
+              <label className='block text-gray-700'>Password</label>
+              <input
+                type='password'
+                className='w-full p-2 border border-gray-300 rounded-lg mt-1'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button
+              type='submit'
+              className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg'
+            >
+              {isRegistering ? 'Register' : 'Login'}
+            </button>
+          </form>
           <button
-            type='submit'
-            className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg'
+            onClick={() => setIsRegistering(!isRegistering)}
+            className='mt-4 text-blue-500 hover:underline'
           >
-            {isRegistering ? 'Register' : 'Login'}
+            {isRegistering
+              ? 'Already have an account? Login'
+              : "Don't have an account? Register"}
           </button>
-        </form>
-        <button
-          onClick={() => setIsRegistering(!isRegistering)}
-          className='mt-4 text-blue-500 hover:underline'
-        >
-          {isRegistering
-            ? 'Already have an account? Login'
-            : "Don't have an account? Register"}
-        </button>
+        </div>
       </div>
     </div>
   );
